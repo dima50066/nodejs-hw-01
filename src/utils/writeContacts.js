01ef8d54962bbcc33eee55ec06aaeab3a1c4bbe0
Dumitru <dima50066@gmail.com>
2024-09-13 16:38:00 +0200

    step 2

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { writeFile } from 'fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const data = JSON.stringify(contacts, null, 2);
+    await writeFile(PATH_DB, data, 'utf-8');
+    console.log('Contacts saved');
+  } catch (error) {
+    console.log('Error writing contacts: ', error);
+    throw new Error('Failed to write contacts');
+  }
+};
