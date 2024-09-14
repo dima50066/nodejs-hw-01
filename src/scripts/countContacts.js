0c04c4a62ee95a628b6e26d03cfba38847257ded
Dumitru <dima50066@gmail.com>
2024-09-14 15:04:57 +0200

    step 6

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,16 @@
-export const countContacts = async () => {};
+import { readContacts } from '../../src/utils/readContacts.js';
+
+export const countContacts = async () => {
+  try {
+    const contacts = await readContacts();
+    const count = contacts.length;
+    console.log(`Total contacts: ${count}`);
+
+    return count;
+  } catch (error) {
+    console.error('Error counting contacts:', error);
+    throw new Error('Failed to count contacts');
+  }
+};
 
 console.log(await countContacts());
