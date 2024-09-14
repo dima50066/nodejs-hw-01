b4904a114e4575eb503435b0ddb003c993ebd556
Dumitru <dima50066@gmail.com>
2024-09-14 14:28:11 +0200

    5 step

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,14 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../../src/utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    const contacts = await readContacts();
+    console.log(contacts);
+    return contacts;
+  } catch (error) {
+    console.log(error);
+    throw new Error('Failed to read contacts');
+  }
+};
 
 console.log(await getAllContacts());
