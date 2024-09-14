e79552bc333d3293c3b7e512171978c4b178b16e
Dumitru <dima50066@gmail.com>
2024-09-14 15:08:24 +0200

    step 7

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,16 @@
-export const removeAllContacts = async () => {};
+import { readContacts } from '../../src/utils/readContacts.js';
+import { writeContacts } from '../../src/utils/writeContacts.js';
+
+export const removeAllContacts = async () => {
+  try {
+    const contacts = await readContacts();
+    const updatedContacts = contacts.slice(0, 0);
+    await writeContacts(updatedContacts);
+    console.log('All contacts successfully removed');
+  } catch (error) {
+    console.log('Error removing contacts:', error);
+    throw new Error('Failed to remove contacts');
+  }
+};
 
 removeAllContacts();
